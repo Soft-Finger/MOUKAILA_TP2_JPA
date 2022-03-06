e63e6d0a6ea693d5cdd7ee3e6c5d4551a791be34
System Administrator <root@MacMikhail.local>
2022-03-06 14:28:25 -0500

    mis à jour de la class bibliotheque.java pour test la creation d'une amende dans AmendeRepoDAO

--- src/main/java/com/tp2/Bibliotheque.java
+++ src/main/java/com/tp2/Bibliotheque.java
@@ -1,15 +1,30 @@
 package com.tp2;
 
-import com.tp2.model.Users;
-import com.tp2.persistence.UsersRepoDAO;
-import com.tp2.service.UserService;
+import com.tp2.model.Amende;
+import com.tp2.persistence.AdminRepoDAO;
+import com.tp2.persistence.AmendeRepoDAO;
+import com.tp2.persistence.ClientRepoDAO;
+import com.tp2.persistence.EmployeRepoDAO;
+import com.tp2.service.AdminService;
+import com.tp2.service.EmployeService;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class Bibliotheque {
     public static void main ( String[] args ) {
-        UserService userService = new UserService( new UsersRepoDAO() );
+        AdminService adminService = new AdminService( new ClientRepoDAO(), new AdminRepoDAO(), new EmployeRepoDAO() );
+        EmployeService employeService = new EmployeService( new AmendeRepoDAO() );
 
-        userService.createEmploye( "Mike", "Wise" );
-        userService.createClient( "Ron", "Paul" );
-        userService.createAdmin( "Bruce", "Wallas" );
+        Amende amende1 = employeService.createAmende( 25.1, true);
+        Amende amende2 = employeService.createAmende( 25.1, true);
+        Set<Amende> amendeSet = new HashSet<Amende>();
+        amendeSet.add( amende1 );
+        amendeSet.add( amende2 );
+
+
+
+
+        adminService.createClient( "Mike", "wise", "1233 rue val", amendeSet,   );
     }
 }
