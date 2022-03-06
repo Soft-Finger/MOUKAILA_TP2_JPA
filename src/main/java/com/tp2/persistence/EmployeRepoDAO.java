62e268b7b4777e74db88b6bcb453905be4c53a0b
System Administrator <root@MacMikhail.local>
2022-03-06 15:45:47 -0500

    mis à jour de la class EmployeRepoDAO pour la cresattion d'une Amend, avec la méthode saveAmnede

--- src/main/java/com/tp2/persistence/EmployeRepoDAO.java
+++ src/main/java/com/tp2/persistence/EmployeRepoDAO.java
@@ -1,4 +1,26 @@
 package com.tp2.persistence;
 
-public class EmployeRepoDAO {
+import com.tp2.model.Amende;
+import org.hibernate.tuple.entity.EntityTuplizerFactory;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EmployeRepoDAO implements EmployeRepository {
+
+    private EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory( "bibliothequeTp2" );
+
+    public void saveAmende ( Amende amende ) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        try {
+            entityManager.getTransaction().begin();
+            entityManager.persist( amende );
+        } catch (Exception e) {
+            e.printStackTrace();
+            entityManager.getTransaction().rollback();
+        } finally {
+            entityManager.close();
+        }
+    }
 }
