867bf861c3857d48b88e7f651e8dde4aab9d5ab6
System Administrator <root@MacMikhail.local>
2022-03-13 07:18:39 -0400

    Mise à jour class DvdRepoDAO pour la persistence d'un dvd

--- src/main/java/com/tp2/persistence/DvdRepoDAO.java
+++ src/main/java/com/tp2/persistence/DvdRepoDAO.java
@@ -1,4 +1,20 @@
 package com.tp2.persistence;
 
-public class DvdRepoDAO {
+import com.tp2.model.Dvd;
+
+import javax.persistence.EntityManager;
+
+import static com.tp2.Manager.emf;
+
+public class DvdRepoDAO implements DvdRepository {
+    @Override
+    public void saveDvd( Dvd dvd) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        em.persist(dvd);
+
+        em.getTransaction().commit();
+        em.close();
+    }
 }
