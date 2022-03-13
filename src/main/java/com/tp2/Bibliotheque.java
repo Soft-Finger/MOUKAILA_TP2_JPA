b78d5a2695260a7b11dca2150d4bf117350837c5
System Administrator <root@MacMikhail.local>
2022-03-13 13:26:50 -0400

    Ajout de la class calculerDateretour(), pour pouvoir attribuer un nombre de jours à chaque type d'emprunteur

--- src/main/java/com/tp2/Bibliotheque.java
+++ src/main/java/com/tp2/Bibliotheque.java
@@ -6,29 +6,22 @@ import com.tp2.service.*;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
 public class Bibliotheque {
-    public static void main ( String[] args ) throws ParseException {
-        EmployeService employeService = new EmployeService( new EmployeRepoDAO());
+    public static Date CalculerDateRetour( int nombreJour) throws ParseException {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        cal.add(Calendar.DATE, nombreJour);
 
-        employeService.createEmployeEmprunteur( simpleDateFormat.parse("2020-01-12"), simpleDateFormat.parse("2020-01-26"), simpleDateFormat.parse("2020-01-30") );
-        employeService.createEmployeEmprunteur( simpleDateFormat.parse("2020-01-12"), simpleDateFormat.parse("2020-01-26"), simpleDateFormat.parse("2020-01-30") );
+        return simpleDateFormat.parse(simpleDateFormat.format(cal.getTime()));
+    }
 
-        /*Amende amende1 = employeService.createAmende( 25.1, true);
-        Amende amende2 = employeService.createAmende( 22.1, false);
-        Set<Amende> amendeSet = new HashSet<Amende>();
-        amendeSet.add( amende1 );
-        amendeSet.add( amende2 );
+    public static void main ( String[] args ) throws ParseException {
 
-        System.out.println(amende1);
-        System.out.println(amende2);*/
-
-
-
-
-        //adminService.createClient( "Mike", "wise", "1233 rue val", amendeSet,   );
     }
 }
--- src/main/java/com/tp2/persistence/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tp2.persistence;
-
-
-
-public interface UsersRepository {
-
-}
