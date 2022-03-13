5c55bfce4c5ee9c8d8ac81a85f78d390170d3929
System Administrator <root@MacMikhail.local>
2022-03-13 06:49:46 -0400

    Mise à jour de la class CdReDOA pour la persistence de la class Cd

--- src/main/java/com/tp2/persistence/AmendeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tp2.persistence;
-
-import com.tp2.model.Amende;
-
-public interface AmendeRepository {
-    void saveAmende( Amende amende );
-}
--- src/main/java/com/tp2/persistence/CdRepoDAO.java
+++ src/main/java/com/tp2/persistence/CdRepoDAO.java
@@ -1,4 +1,20 @@
 package com.tp2.persistence;
 
-public class CdRepoDAO {
+import com.tp2.model.Cd;
+
+import javax.persistence.EntityManager;
+
+import static com.tp2.Manager.emf;
+
+public class CdRepoDAO implements CdRepository {
+    @Override
+    public void saveCd( Cd cd) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        em.persist(cd);
+
+        em.getTransaction().commit();
+        em.close();
+    }
 }
