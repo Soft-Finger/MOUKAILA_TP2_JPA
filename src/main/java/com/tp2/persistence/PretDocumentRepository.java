9314ed30b28d9709706c45c8239c90c4f334f84e
System Administrator <root@MacMikhail.local>
2022-03-13 07:28:22 -0400

    sauvegarde d'un pret et recherche de tous les prets dans l'interface PretDocumentRepository

--- src/main/java/com/tp2/persistence/PretDocumentRepository.java
+++ src/main/java/com/tp2/persistence/PretDocumentRepository.java
@@ -1,4 +1,10 @@
 package com.tp2.persistence;
 
-public class PretDocumentRepository {
+import com.tp2.model.Client;
+import com.tp2.model.PretDocument;
+
+public interface PretDocumentRepository {
+    void savePret( PretDocument pretDoc);
+
+    void findAllPretDocument( Client client);
 }
