8b4058a414898873ead438b29f3e07d632316325
System Administrator <root@MacMikhail.local>
2022-03-13 06:40:10 -0400

    Mise à jour des class EmployeService pour d'un client, d'un livre, d'un cd, d'un dvd et d'un pret

--- src/main/java/com/tp2/service/EmployeService.java
+++ src/main/java/com/tp2/service/EmployeService.java
@@ -1,19 +1,129 @@
 package com.tp2.service;
 
-import com.tp2.model.Amende;
-import com.tp2.persistence.AmendeRepository;
-import com.tp2.persistence.EmployeRepository;
+import com.tp2.model.*;
+import com.tp2.persistence.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class EmployeService {
-    private EmployeRepository employeRepository;
+    private ClientRepository clientRepository;
+    private LivreRepository livreRepository;
+    private CdRepository cdRepository;
+    private DvdRepository dvdRepository;
+    private DocumentRepository documentRepository;
+    private PretDocumentRepository pretDocumentRepository;
 
-    public EmployeService ( EmployeRepository employeRepository ) {
-        this.employeRepository = employeRepository;
+    public EmployeService( ClientRepository clientRepo , LivreRepository livreRepo , CdRepository cdRepo , DvdRepository dvdRepo , DocumentRepository documentRepo , PretDocumentRepository pretDocumentRepo ) {
+        this.clientRepository = clientRepository;
+        this.livreRepository = livreRepo;
+        this.cdRepository = cdRepository;
+        this.dvdRepository = dvdRepository;
+        this.documentRepository = documentRepository;
+        this.pretDocumentRepository = pretDocumentRepository;
     }
 
-    public long createEmployeEmprunteur( Date datePret, Date dateRetour, Date dateRetourEffectif) {
-        return employeRepository.createClientEmprunteur( datePret, dateRetour, dateRetourEffectif );
+    public void createClient(String nom, String prenom, String adresse) {
+        Client client = Client.builder()
+                .nom(nom)
+                .prenom(prenom)
+                .adresse(adresse)
+                .build();
+        clientRepository.saveClient(client);
     }
+
+    public void createLivre(String titre, String auteur, String editeur, Date datePub, String genre, boolean disponible, int dureeMaxpret, int nbPages) {
+        Livre livre = Livre.builder()
+                .titre(titre)
+                .auteur(auteur)
+                .editeur(editeur)
+                .datePub(datePub)
+                .genre(genre)
+                .disponible(disponible)
+                .dureeMaxpret(dureeMaxpret)
+                .nbPages(nbPages)
+                .build();
+        livreRepository.saveLivre(livre);
+    }
+
+    public void createCd(String titre, String auteur, String editeur, Date datePub, String genre, boolean disponible, int dureeMaxpret, int duree) {
+        Cd cd = Cd.builder()
+                .titre(titre)
+                .auteur(auteur)
+                .editeur(editeur)
+                .datePub(datePub)
+                .genre(genre)
+                .disponible(disponible)
+                .dureeMaxpret(dureeMaxpret)
+                .duree(duree)
+                .build();
+        cdRepository.saveCd(cd);
+    }
+
+    public void createDvd(String titre, String auteur, String editeur, Date datePub, String genre, boolean disponible, int dureeMaxpret, int duree) {
+        Dvd dvd = Dvd.builder()
+                .titre(titre)
+                .auteur(auteur)
+                .editeur(editeur)
+                .datePub(datePub)
+                .genre(genre)
+                .disponible(disponible)
+                .dureeMaxpret(dureeMaxpret)
+                .duree(duree)
+                .build();
+        dvdRepository.saveDvd(dvd);
+    }
+
+    public List<Document> findByTitre( String titre) {
+        return documentRepository.findByTitre(titre);
+    }
+
+    public List<Document> findByAuteur(String aut) {
+        return documentRepository.findByAuteur(aut);
+    }
+
+    public List<Document> findByCategorie(String genre) {
+
+        return documentRepository.findByCategorie(genre);
+    }
+
+    public List<Document> findByAnnee(String anneePub) {
+        return documentRepository.findByAnnee(anneePub);
+    }
+
+    public static Date CalculerDateRetour(int nombreJour) throws ParseException {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Date());
+        cal.add(Calendar.DATE, nombreJour);
+
+        return simpleDateFormat.parse(simpleDateFormat.format(cal.getTime()));
+    }
+
+    public void createPret(Date datePret, Client client, Document document) throws ParseException {
+        PretDocument pret = PretDocument.builder()
+                .datePret(datePret)
+                .dateRetourEffectif(null)
+                .dateRetour(CalculerDateRetour(document.getDureeMaxpret()))
+                .client(client)
+                .document(document)
+                .build();
+        pretDocumentRepository.savePret(pret);
+    }
+
+    public Client findClientById(long clientId) {
+        return clientRepository.findClientById(clientId);
+    }
+
+    public Document findDocumentById(long documentId) {
+        return documentRepository.findDocumentById(documentId);
+    }
+
+    public void findAllPretDocument(Client client) {
+        pretDocumentRepository.findAllPretDocument(client);
+    }
+
 }
