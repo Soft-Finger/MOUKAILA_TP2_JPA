5fd4c3846e4832910c0eacc1e7ae951826eb0b7d
System Administrator <root@MacMikhail.local>
2022-03-13 07:25:04 -0400

    MÀJ persistence de d'un livre

--- src/main/java/com/tp2/persistence/LivreRepoDAO.java
+++ src/main/java/com/tp2/persistence/LivreRepoDAO.java
@@ -1,4 +1,21 @@
 package com.tp2.persistence;
 
-public class LivreRepoDAO {
+import com.tp2.model.Livre;
+
+import javax.persistence.EntityManager;
+
+import static com.tp2.Manager.emf;
+
+public class LivreRepoDAO implements LivreRepository {
+
+    @Override
+    public void saveLivre( Livre livre) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        em.persist(livre);
+
+        em.getTransaction().commit();
+        em.close();
+    }
 }
