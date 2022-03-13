c2625b9c31265e82d9dcf89c20cf32a3519a5135
System Administrator <root@MacMikhail.local>
2022-03-13 11:15:38 -0400

    Avanr de persiter un document dans la base de donnée je verifie d'abord sa disponibilité avec la méthode disponibiliteDoc

--- src/main/java/com/tp2/persistence/PretDocumentRepoDAO.java
+++ src/main/java/com/tp2/persistence/PretDocumentRepoDAO.java
@@ -1,4 +1,36 @@
 package com.tp2.persistence;
 
-public class PretDocumentRepoDAO {
+import com.tp2.model.Client;
+import com.tp2.model.Document;
+import com.tp2.model.PretDocument;
+
+import javax.persistence.EntityManager;
+
+import static com.tp2.Manager.emf;
+
+public class PretDocumentRepoDAO implements PretDocumentRepository {
+
+    public boolean disponibiliteDoc( Document doc) {
+        EntityManager em = emf.createEntityManager();
+        boolean test;
+        em.getTransaction().begin();
+
+        Document d = em.find( Document.class , doc.getIdDoc() );
+        test = d.isDisponible();
+        em.getTransaction().commit();
+        em.close();
+        return test;
+    }
+
+    @Override
+    public void savePret ( PretDocument pretDoc ) {
+
+
+
+    }
+
+    @Override
+    public void findAllPretDocument ( Client client ) {
+
+    }
 }
